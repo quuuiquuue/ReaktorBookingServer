b439c6d8fb62882b712734b15f262887dd33e71f
quuuiquuue <econlop014@g.educaand.es>
2024-11-18 14:10:07 +0100

    Structure completed

---
+++ reaktor_booking_server/src/main/java/es/iesjandula/reaktor_booking_server/ReaktorBookingServerApplication.java
@@ -0,0 +1,13 @@
+package es.iesjandula.reaktor_booking_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaktorBookingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaktorBookingServerApplication.class, args);
+	}
+
+}
