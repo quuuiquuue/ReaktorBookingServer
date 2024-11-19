8df480bceb2c8608049b4abc812222a99d1c1a1c
Pgenrique7 <pgenrique7@gmail.com>
2024-11-19 16:59:39 +0100

    Aula model

--- src/main/java/es/iesjandula/reaktor_booking_server/models/Aula.java
+++ src/main/java/es/iesjandula/reaktor_booking_server/models/Aula.java
@@ -1,15 +1,22 @@
 package es.iesjandula.reaktor_booking_server.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Aula {
+	
+	@EmbeddedId
+	private AulaId AulaId;
 	
 
 }
