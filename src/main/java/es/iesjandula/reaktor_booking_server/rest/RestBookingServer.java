d154ec738f20d7913f7b3eb99a5cff69fbb12871
Pgenrique7 <pgenrique7@gmail.com>
2024-11-19 18:02:42 +0100

    RestBookingServer

---
+++ src/main/java/es/iesjandula/reaktor_booking_server/rest/RestBookingServer.java
@@ -0,0 +1,12 @@
+package es.iesjandula.reaktor_booking_server.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class RestBookingServer {
+	
+
+	
+}
